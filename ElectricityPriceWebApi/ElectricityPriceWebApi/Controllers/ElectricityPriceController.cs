732183b4d81bd76d18b5214a244d062b4669c0c8
Miika Hämäläinen <miika.hamalainen@hotmail.fi>
2022-09-12 21:10:05 +0300

    Add ElectricityPriceWebApi solution and project + initial EF Models.

---
+++ ElectricityPriceWebApi/ElectricityPriceWebApi/Controllers/ElectricityPriceController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ElectricityPriceWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ElectricityPriceController : ControllerBase
+    {
+    }
+}
